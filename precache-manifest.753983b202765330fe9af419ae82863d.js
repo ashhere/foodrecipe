7f1a488890886a41f8aa3f4d9de70c5722702c2c
ashhere <ashok.ashock98@gmail.com>
2021-02-21 18:35:45 +0545

    Updates

---
+++ precache-manifest.753983b202765330fe9af419ae82863d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8da35120fc0b8588191",
+    "url": "/foodrecipe/static/css/main.2ce342e3.chunk.css"
+  },
+  {
+    "revision": "f8da35120fc0b8588191",
+    "url": "/foodrecipe/static/js/main.d81dc611.chunk.js"
+  },
+  {
+    "revision": "91018978ee7503d771a2",
+    "url": "/foodrecipe/static/js/runtime~main.35212895.js"
+  },
+  {
+    "revision": "55ae8b6cd712fb9c52af",
+    "url": "/foodrecipe/static/js/2.7135fb03.chunk.js"
+  },
+  {
+    "revision": "2e190fba2236f6820919c24bb51c4a58",
+    "url": "/foodrecipe/index.html"
+  }
+];
